14f54bf0db9b0948610140702989ce7675b3c17b
Aries Cs <aries0d0f@gmail.com>
2021-03-14 13:19:51 +0800

    deploy: Modify root mount selector.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lkc-root',
+  selector: '[lkc-root]',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
