163e7eae2aa49dfca57013db68ca7bbb500beca0
Aries Cs <aries0d0f@gmail.com>
2021-03-14 13:22:01 +0800

    feat(route): Add app routing rules.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: 'news',
+    loadChildren: () =>
+      import('./pages/news/news.module').then((m) => m.NewsModule)
+  },
+  {
+    path: 'product',
+    loadChildren: () =>
+      import('./pages/product/product.module').then((m) => m.ProductModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
