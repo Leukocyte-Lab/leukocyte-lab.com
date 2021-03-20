b81287e547b1880dac64ef09770adbde31f45f6e
Aries Cs <aries0d0f@gmail.com>
2021-03-20 13:49:52 +0800

    fix: Load `ThemeModule` into home module.

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { ThemeModule } from 'src/app/@theme/theme.module';
 
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, ThemeModule, HomeRoutingModule],
   declarations: [HomeComponent],
   providers: []
 })
