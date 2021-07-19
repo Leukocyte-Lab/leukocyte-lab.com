1101ebc00a0eb7576c0989b3d07880d152008000
Aries Cs <aries0d0f@gmail.com>
2021-07-19 10:51:04 +0800

    feat(module): Load `ThemeModule` into `Product`.

--- src/app/pages/product/product.module.ts
+++ src/app/pages/product/product.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
+import { ThemeModule } from 'src/app/@theme/theme.module';
 
 import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  imports: [CommonModule, ProductRoutingModule],
+  imports: [CommonModule, ThemeModule, ProductRoutingModule],
   declarations: [ProductComponent],
   providers: []
 })
